c1f6b107ebe1f2df69066f3a03336258f95d5306
Mackinley <mackinley@punchbuggy.com.au>
2024-09-02 09:19:20 +1000

    restore ref import

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -181,7 +181,7 @@ ul {
 </style>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
+import { Component, Vue, ref  } from 'nuxt-property-decorator';
 import settings from '@/content/settings/general.json';
 
 export default class Home extends Vue {
